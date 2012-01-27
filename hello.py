5c4861e86d28dbfe9f77cb630cfb4e85ff62f2bd
Peng Yong <ppyy@juyide.com>
2012-01-27 16:06:05 +0800

    print date of today

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from datetime import datetime
+from datetime import datetime, date
 
 print "hello world, ppyy!"
 print "hello world!"
 print "datetime: %s" % datetime.now()
+print "date: %s" % date.today()
