4707d9a5d4101f8f757e6ce69fb109df22dfcae3
Peng Yong <ppyy@pubyun.com>
2012-10-29 14:43:56 +0800

    delete unused helloword statement
    Change-Id: Iade8d6f0d408e16d1924b5441b4a0864e8320534

--- hello.py
+++ hello.py
@@ -4,7 +4,6 @@ from datetime import datetime, date
 
 print "begin"
 print "hello world, gerrit2!"
-print "hello world!"
 print "datetime: %s" % datetime.now()
 print "date: %s" % date.today()
 print "end"
